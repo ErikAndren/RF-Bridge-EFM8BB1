b3cac5160f0229efecee0c9b6f9e7ae3295be5d5
Erik Andrén Zachrisson <erik@zachrisson.info>
2018-02-28 20:48:10 +0100

    Update test case to pass or fail

--- test/learning_time_out.py
+++ test/learning_time_out.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import serial
+import binascii
 from optparse import OptionParser
 
 parser = OptionParser()
@@ -8,8 +9,24 @@ parser.add_option("-p", "--port", dest="port", help="serial port to use")
 
 (options, args) = parser.parse_args()
 
-ser = serial.Serial(options.port, 19200, timeout=1);
+ser = serial.Serial(options.port, 19200, timeout=35);
 
+ser.flush()
 
-print(ser.name)
+learn_cmd = 'AAA155'
+ser.write(learn_cmd.decode("hex"))
+
+ack = ser.read(3)
+print ack.encode('hex_codec')
+
+# Wait for timeout
+ack = ser.read(3)
+
+if ack == binascii.unhexlify('aaa255'):
+    print("PASS: Received expected value 0xaaa255")
+else:
+     print("FAIL: Received " + binascii.hexlify(ack) + ", expected value 0xaaa255")
+
+#print ack.encode('hex_codec')
+
 ser.close()
